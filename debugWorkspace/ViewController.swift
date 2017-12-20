96ead131aa7c93e45a96682b9de3e3ee58a59f88
hiriorima <g2117001@fun.ac.jp>
2017-12-20 22:25:48 +0900

    Initial Commit

---
+++ debugWorkspace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  debugWorkspace
+//
+//  Created by 会津慎弥 on 2017/12/20.
+//  Copyright © 2017年 会津慎弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
